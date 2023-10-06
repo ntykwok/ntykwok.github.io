f1693f69f1806a6aa2101cb27cbd5a37ca296929
Nicholas Kwok <85721398+ntykwok@users.noreply.github.com>
2023-10-06 14:24:50 +0000

    2

--- app/page.js
+++ app/page.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-purple-500 to-blue-500 justify-center items-center">
       <Head>
-        <title>Nicholas Kwok's Personal Website</title>
+        <title>Nicholas Kwok&apos;s Personal Website</title>
         <meta name="description" content="Nicholas Kwok's Personal Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col justify-center items-center flex-1 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to Nicholas Kwok's Personal Website
+          Welcome to Nicholas Kwok&apos;s Personal Website
         </h1>
 
         <p className="mt-3 text-2xl">
@@ -20,7 +20,7 @@ export default function Home() {
 
         <div className="flex items-center justify-center mt-6">
           <a href="/contact" className="m-3 p-2 border-2 border-grey-100 rounded text-2xl text-grey-300 hover:bg-white-500 hover:text-white transition-all">
-            Don't be shy, push me!
+            Don&apos;t be shy, push me!
           </a>
         </div>
       </main>
